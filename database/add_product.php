e738a655abea8a2bc711a4eefb736a0d2f7e0ee1
ADMIN <djaeyeo@gmail.com>
2024-03-27 09:09:53 +0900

    파일 수정

--- database/add_product.php
+++ database/add_product.php
@@ -1,19 +1,19 @@
 <?php
-$servername = "10.114.13.79";
-$username = "wogud0501";
-$password = "wogud05041^";
-$dbname = "shop";
+require_once "init.php";
+
 // 상품 정보 수신
+global $productDoc, $productName, $available;
 $productDoc = $_POST['productDoc'];
 $productName = $_POST['productName'];
 $available = $_POST['available'];
 
 // MySQL에 연결
+global $servername, $username, $password, $dbname;
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // 연결 확인
 if ($conn->connect_error) {
-die("연결 실패: " . $conn->connect_error);
+    die("연결 실패: " . $conn->connect_error);
 }
 
 // SQL 쿼리 작성
@@ -22,10 +22,11 @@ VALUES ('$productDoc', '$productName', '$available')";
 
 // 쿼리 실행
 if ($conn->query($sql) === TRUE) {
-echo "상품이 성공적으로 추가되었습니다.";
+    echo "success";
 } else {
-echo "상품 추가 실패: " . $conn->error;
+    echo "error";
 }
 
 // MySQL 연결 닫기
 $conn->close();
+?>
---
+++ database/init.php
@@ -0,0 +1,6 @@
+<?php
+global $servername, $username, $password, $dbname;
+$servername = "10.114.13.79";
+$username = "wogud0501";
+$password = "wogud05041^";
+$dbname = "shop";
