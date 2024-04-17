a84fb8aefaad198be4499de044144fb823e553ab
ADMIN <djaeyeo@gmail.com>
2024-04-17 09:21:57 +0900

    edited html

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+var selectedRow =  0;
+function changeRow(rowNumber) {
+    selectedRow = rowNumber;
+    alert("행 " + rowNumber + "선택");
+}
+
 function addProduct() {
-    var productDoc = document.getElementById("productDoc").value;
-    var productName = document.getElementById("productName").value;
+    var productDoc = document.getElementById("productName").value;
+    var productName = document.getElementById("productDoc").value;
     var available = document.getElementById("available").value;
+
+    var firstRow = document.querySelector('tbody tr');
+
+    var cells = firstRow.getElementsByTagName('td')
+
+    cells[1].textContent = productName;
+    cells[2].textContent = productDoc;
+    cells[3].textContent = available;
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            alert(this.responseText); // 서버로부터의 응답을 알림으로 표시
+            alert(this.responseText); // 서버로부터의 응답'을 알림으로 표시
         }
     };
-    xhttp.open("POST", "connect.php", true);
+    xhttp.open("POST", "./add_product.php", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     // POST 요청으로 상품 정보를 전송
-    xhttp.send("productDoc=" + productDoc + "&productName=" + productName + "&available=" + available);
+    xhttp.send("productName=" + productName + "&productDoc=" + productDoc  + "&available=" + available);
 }
