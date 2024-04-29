e8274d5fe88c514e98516317db7f18ec02a7bb31
ADMIN <djaeyeo@gmail.com>
2024-04-29 09:41:55 +0900

    edited index.html script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* index 배열 정의*/
 var rowData = [
     { location : ' ', name: ' ', count: ' ' },
     { location : ' ', name: ' ', count: ' ' },
@@ -5,13 +6,29 @@ var rowData = [
     { location : ' ', name: ' ', count: ' ' },
     { location : ' ', name: ' ', count: ' ' }
 ];
-
-document.addEventListener("click" , function(event) {
-    if  ( event.target.matches("#productLoc , #productName, #available") ) {
-        document.getElementById("row1Btn").style.display = "inline-block";
-        document.getElementById("row2Btn").style.display = "inline-block";
-        document.getElementById("row3Btn").style.display = "inline-block";
-        document.getElementById("row4Btn").style.display = "inline-block";
-        document.getElementById("row5Btn").style.display = "inline-block";
+/* 버튼 활성화 */
+document.addEventListener("click", function(event) {
+    if (event.target.matches("#productLoc, #productName, #available")) {
+        for (let i = 1; i <= 5; i++) {
+            document.getElementById("row" + i + "Btn").style.display = "inline-block";
+        }
     }
+});
+
+function changeRow(rowNum) {
+    alert(rowNum + "번째 행이 변경 되었습니다!");
+}
+/* Update row */
+function updateRow() {
+    var productLoc = document.getElementById("productLoc").value;
+    var productName = document.getElementById("productName").value;
+    var available = document.getElementById("available").value;
+
+    var selectedRow = getSelectedRow();
+
+    rowData[selectedRow].location = productLoc;
+    rowData[selectedRow].name = productName;
+    rowData[selectedRow].count = available;
+
+    /* Update the table */
 }
